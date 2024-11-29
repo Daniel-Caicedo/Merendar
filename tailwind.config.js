ee20feeecebe303cbf9c66c4bcaa5fb858f08ef2
Daniel Caicedo <danielfcaicedootmail.com>
2024-11-29 14:04:17 -0500

    se le agregan animaciones a los objetos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import animations from '@midudev/tailwind-animations'
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}" ,
            "/node_modules/preline/preline.js",],
@@ -8,6 +11,7 @@ export default {
   },
   plugins: [
     require ('preline/plugin'),
+    animations,
     
   ],
 }
